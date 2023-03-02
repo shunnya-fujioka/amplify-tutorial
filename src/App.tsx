dad181ccdad286ffdef7f62659d8a7bd28fed3bd
shunnya-fujioka <125859014+shunnya-fujioka@users.noreply.github.com>
2023-03-02 00:37:45 +0000

    画像のアップロード・表示機能を追加

--- src/App.tsx
+++ src/App.tsx
@@ -6,19 +6,21 @@ import {
   View,
 } from "@aws-amplify/ui-react";
 import { useState, useEffect } from 'react';
-import { API, graphqlOperation } from 'aws-amplify';
+import { API, graphqlOperation, Storage } from 'aws-amplify';
 import { listNotes } from './graphql/queries';
 import { createNote as createNoteMutation, deleteNote as deleteNoteMutation } from './graphql/mutations';
 import { ListNotesQuery, CreateNoteInput, DeleteNoteInput } from './API'
 import { GraphQLResult } from '@aws-amplify/api-graphql';
 
-const initialFormState: CreateNoteInput = { name: '', description: '' }
+const initialFormState: CreateNoteInput = { name: '', description: '', imageName: '' }
 
 interface Props extends WithAuthenticatorProps { }
 
 interface Note extends CreateNoteInput {
   // optional into required
   id: string;
+  // `Storage.get()`で取得した値を格納
+  imageSrc?: string | null | undefined;
 }
 
 function App({ signOut }: Props) {
@@ -32,37 +34,56 @@ function App({ signOut }: Props) {
   async function fetchNotes() {
     const apiData = await API.graphql(graphqlOperation(listNotes)) as GraphQLResult<ListNotesQuery>;
     if (apiData.data?.listNotes?.items) {
-      const notes = apiData.data.listNotes.items as Note[];
+      const notesFromAPI = apiData.data.listNotes.items;
+      const notes = await Promise.all(notesFromAPI.map(async note => {
+        const imageSrc = note?.imageName && await Storage.get(note.imageName);
+        return { ...note, imageSrc }
+      })) as Note[];
       setNotes(notes);
     }
   }
 
   async function createNote() {
-    if (!formData.name || !formData.description) return;
+    if (!formData.name) return;
     await API.graphql(graphqlOperation(createNoteMutation, { input: formData }));
     fetchNotes();
     setFormData(initialFormState);
   }
 
   async function deleteNote({ id }: DeleteNoteInput) {
-    const newNotesAray = notes.filter(note => note.id !== id);
-    setNotes(newNotesAray);
+    const newNotesArray = notes.filter(note => note.id !== id);
+    setNotes(newNotesArray);
     await API.graphql(graphqlOperation(deleteNoteMutation, { input: { id } }));
+  }
+
+  async function uploadImage(e: React.ChangeEvent<HTMLInputElement>) {
+    if (!e.target.files) return;
+    const file = e.target.files[0];
+    setFormData({ ...formData, imageName: file.name });
+    await Storage.put(file.name, file);
+    fetchNotes();
   }
 
   return (
     <View className="App">
       <h1>My Notes App</h1>
       {/* ノート作成 */}
+      {/* 名前 */}
       <input
         onChange={e => setFormData({ ...formData, 'name': e.target.value })}
         placeholder="Note name"
         value={formData.name}
       />
+      {/* 内容 */}
       <input
         onChange={e => setFormData({ ...formData, 'description': e.target.value })}
         placeholder="Note description"
         value={formData.description || ''}
+      />
+      {/* 画像 */}
+      <input
+        type="file"
+        onChange={uploadImage}
       />
       <button onClick={createNote}>Create Note</button>
       {/* 各ノート表示 */}
@@ -71,6 +92,7 @@ function App({ signOut }: Props) {
           <div key={note.id}>
             <h2>{note.name}</h2>
             <p>{note.description}</p>
+            {note.imageSrc && <img src={note.imageSrc} style={{ width: 400 }} alt="" />}
             {/* ノート削除 */}
             <button onClick={() => deleteNote({ id: note.id })}>Delete note</button>
           </div>
